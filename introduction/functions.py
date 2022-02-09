35ede81e9b3eec2af36ad9f57f848144ea4df4a0
lKOAl <justinnguyen25@ymail.com>
2022-02-09 17:15:53 -0600

    commit

---
+++ introduction/functions.py
@@ -0,0 +1,10 @@
+def sample_function_name(): #(parameter)
+    
+    x = 10
+    while x >= 0:
+        print(x)
+        x-= 1
+    
+    return ("I'm a sample function!")
+
+sample_function_name() #(argument)
